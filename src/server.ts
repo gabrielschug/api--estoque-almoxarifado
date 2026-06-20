77548153bcf8302378f133ad4d637f7261166fd5
Gabriel Schug <gabrielschug@hotmail.com>
2026-06-20 18:38:34 -0300

    :sparkles: add server

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+import routesFornecedores from "./routes/fornecedores"
+import routesSecretarias from "./routes/secretarias"
+import routesProdutos from "./routes/produtos"
+import routesEntradas from "./routes/entradas"
+import routesSaidas from "./routes/saidas"
+import routesRelatorios from "./routes/relatorios"
+
+
+app.use(express.json())
+
+app.use("/fornecedores", routesFornecedores)
+app.use("/secretarias", routesSecretarias)
+app.use("/produtos", routesProdutos)
+app.use("/entradas", routesEntradas)
+app.use("/saidas", routesSaidas)
+app.use("/relatorios", routesRelatorios)
+
+
+app.get('/', (req, res) => {
+  res.send('API: Sistema de Estoque')
+})
+
+app.listen(port, () => {
+  console.log(`Servidor Rodando na Porta: ${port}`)
+})
